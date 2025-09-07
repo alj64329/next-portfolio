471a65b762a0a9576c3821bd8c9c42c63f768372
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-09-07 14:31:48 -0700

    first commit

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+
+        <div>
+            <div className='heading-text text-7xl text-center'>Web
+            <br/><span className='uppercase'>Developer</span>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Hero
