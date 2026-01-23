63a0b99b7c9a43fef2fd3d6260657fb1ac4b757e
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2026-01-22 20:30:45 -0800

    Laptop.tsx type implemented

--- src/app/components/Laptop.tsx
+++ src/app/components/Laptop.tsx
@@ -1,26 +1,28 @@
 'use client'
-import React, { useRef, useState } from 'react'
+import React, { JSX, useRef, useState } from 'react'
+import * as THREE from 'three'
 import { Float, useGLTF, useTexture } from '@react-three/drei'
 
-const Laptop =(props)=> {
-  const { nodes, materials } = useGLTF('/models/laptop.glb')
+type LaptopProps = JSX.IntrinsicElements['group']
 
+const Laptop =(props:LaptopProps)=> {
+  const { nodes, materials } = useGLTF('/models/laptop.glb')
   const screenTexture = useTexture('/texture/screen.png')
 
   return(
-    <Float dispose={1}>
+    <Float>
       <group position={[-0.186, 1.099, -0.194]} rotation={[0.837, -0.44, 0.154]} {...props}>
         <group position={[0, 0.504, -1.015]} rotation={[-2.172, 0, 0]}>
           <mesh
-            castShadowd
+            castShadow
             receiveShadow
-            geometry={nodes.Object_33.geometry}
+            geometry={(nodes.Object_33 as THREE.Mesh).geometry}
             material={materials.Blue}
           />
           <mesh
             castShadow
             receiveShadow
-            geometry={nodes.Object_34.geometry}
+            geometry={(nodes.Object_34 as THREE.Mesh).geometry}
             material={materials.Black_Dark}
           >
           <meshStandardMaterial  map ={screenTexture}/>
@@ -29,43 +31,43 @@ const Laptop =(props)=> {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_23.geometry}
+          geometry={(nodes.Object_23 as THREE.Mesh).geometry}
           material={materials.Blue}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_24.geometry}
+          geometry={(nodes.Object_24 as THREE.Mesh).geometry}
           material={materials.Orange}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_25.geometry}
+          geometry={(nodes.Object_25 as THREE.Mesh).geometry}
           material={materials.White}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_26.geometry}
+          geometry={(nodes.Object_26 as THREE.Mesh).geometry}
           material={materials.Yellow}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_27.geometry}
+          geometry={(nodes.Object_27 as THREE.Mesh).geometry}
           material={materials.Blue_Dark}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_29.geometry}
+          geometry={(nodes.Object_29 as THREE.Mesh).geometry}
           material={materials['.Example_Sheet_033.001']}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_31.geometry}
+          geometry={(nodes.Object_31 as THREE.Mesh).geometry}
           material={materials['.Example_Sheet_033.001']}
         />
       </group>
